9c705c1b40c23cb7e4b6b048397ebb5e232caed5
Ubhash <111178859+Ubhash@users.noreply.github.com>
2025-04-09 11:18:25 +0530

    add this file
    add this file

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); 
+
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("✅ Connected to MongoDB"))
+    .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
+
+require('./routes.js')(app);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
